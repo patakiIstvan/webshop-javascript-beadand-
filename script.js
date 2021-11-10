ce64372f69ad28b3b2d4725686f39bafb2055bb4
Pataki István <patakiistvan1997y@gmail.com>
2021-11-10 21:44:03 +0100

    A setOrder figyelembe veszi a magyar ABC betuit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Babel is life
+// selectors
 const checkbox = document.querySelector('.sale');
 const productContainer = document.querySelector('.products');
 const products = document.querySelectorAll('.product');
@@ -30,11 +30,13 @@ function setOrder() {
       .forEach(product => productContainer.appendChild(product));
   } else if (select.value == 2) { // ABC növekvő
     [...productContainer.children]
-    .sort((a, b) => a.querySelectorAll('.product-name')[0].innerText.toUpperCase() > b.querySelectorAll('.product-name')[0].innerText.toUpperCase() ? 1 : -1)
+    //.sort((a, b) => a.querySelectorAll('.product-name')[0].innerText.toUpperCase() > b.querySelectorAll('.product-name')[0].innerText.toUpperCase() ? 1 : -1)
+    .sort((a, b) => a.querySelectorAll('.product-name')[0].innerText.localeCompare(b.querySelectorAll('.product-name')[0].innerText))
       .forEach(product => productContainer.appendChild(product));
   } else if (select.value == 3) { // ABC csökkenő
     [...productContainer.children]
-    .sort((a, b) => a.querySelectorAll('.product-name')[0].innerText.toUpperCase() > b.querySelectorAll('.product-name')[0].innerText.toUpperCase() ? 1 : -1).reverse()
+    //.sort((a, b) => a.querySelectorAll('.product-name')[0].innerText.toUpperCase() > b.querySelectorAll('.product-name')[0].innerText.toUpperCase() ? 1 : -1).reverse()
+    .sort((a, b) => a.querySelectorAll('.product-name')[0].innerText.localeCompare(b.querySelectorAll('.product-name')[0].innerText)).reverse()
       .forEach(product => productContainer.appendChild(product));
   }
 }
